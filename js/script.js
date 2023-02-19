05a40f5159e157712db21ea7809200b008985df3
leticea <nascimento.leticeah@gmail.com>
2023-02-19 11:41:10 -0300

    adicionando API

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ selectTag.forEach((tag, id) => {
   }
 });
 
+// Function to change countries when exchange icon click
 exchangeIcon.addEventListener("click", () => {
   let tempText = fromText.value,
     tempLang = selectTag[0].value;
@@ -28,4 +29,33 @@ exchangeIcon.addEventListener("click", () => {
   toText.value = tempText;
   selectTag[0].value = selectTag[1].value;
   selectTag[1].value = tempLang;
+});
+
+fromText.addEventListener("keyup", () => {
+  if (!fromText.value) {
+    toText.value = "";
+  }
+});
+
+// Translate button click events
+translateBtn.addEventListener("click", () => {
+  let text = fromText.value.trim(),
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+  if (!text) return; // do nothing when input is empty
+  toText.setAttribute("placeholder", "Translating...");
+
+  // Adding API
+  let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+  fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      toText.value = data.responseData.translatedText;
+      data.matches.forEach((data) => {
+        if (data.id === 0) {
+          toText.value = data.translation;
+        }
+      });
+      toText.setAttribute("placeholder", "Translation");
+    });
 });
